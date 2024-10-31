ed3041bb66913e4a636ff2c7a9b6ec8feb95cfe2
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-01 02:12:36 +0900

    Feat : url에 CreatePromiseOptionsView 추가

--- PlanPal/urls.py
+++ PlanPal/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('plan/', include('plan.urls'))
+    path('plan/', include('plan.urls')),
+    path('promises/', include('promise.urls')),
 ]
