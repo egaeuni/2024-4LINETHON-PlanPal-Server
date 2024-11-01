ee96899064481e3a19174c2aa67d01f2ad70369a
egaeuni <gaeun3619@gmail.com>
2024-11-01 14:31:28 +0900

    feat:token으로 user 구분

--- plan/views.py
+++ plan/views.py
@@ -8,13 +8,25 @@ class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     permission_classes = [CustomReadOnly]
 
+    def get_queryset(self):
+        return Category.objects.filter(author=self.request.user)
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save()
 
 class PlanViewSet(viewsets.ModelViewSet):
     queryset = Plan.objects.all()
     serializer_class = PlanSerializer
     permission_classes = [CustomReadOnly]
 
+    def get_queryset(self):
+        return Plan.objects.filter(author=self.request.user)
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save()
