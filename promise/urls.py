29dad54aa0a0b335c08ea38e249a3e48913dd4ee
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-01 20:05:43 +0900

    Feat : 약속 id로 삭제 & username으로 약속 조회 구현

--- promise/urls.py
+++ promise/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views.CreatePromiseOptionsView import CreatePromiseOptionsView
 from .views.ConfirmImmediatelyView import ConfirmImmediatelyView
-from .views.PromiseCRUDView import PromiseCRUDView
+from .views.PromiseCRUDView import PromiseCRUDView, GETPromiseByUsername
 
 urlpatterns = [
     path('promise/option/', CreatePromiseOptionsView.as_view(), name='promise-option-create'),
     path('promise/confirm/immediately/<int:promise_id>/<int:promise_option_id>', ConfirmImmediatelyView.as_view(), name='promise-confirm-immediately'),
     path('promise/<int:promise_id>/', PromiseCRUDView.as_view(), name='promise-crud'),
+    path('promise/<str:username>/', GETPromiseByUsername.as_view(), name='promise-list-by-username'),
 ]
