d1e3d0dbb454c70ba4273f97f0121c37bc036100
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-05 06:23:05 +0900

    Feat : 즐겨찾기 선택/해제 구현

--- promise/serializers.py
+++ promise/serializers.py
@@ -25,6 +25,14 @@ class MemoSerializer(serializers.ModelSerializer):
         model = Memo
         fields = ['id', 'user', 'content', 'promise'] 
 
+# Mark 시리얼라이저
+class MarkSerializer(serializers.ModelSerializer):
+    user = ProfileSerializer(read_only=True)
+
+    class Meta:
+        model = Memo
+        fields = ['id', 'user', 'promise'] 
+
 # 약속 시리얼라이저
 class PromiseSerializer(serializers.ModelSerializer):
     user = ProfileSerializer(read_only=True)
@@ -34,9 +42,17 @@ class PromiseSerializer(serializers.ModelSerializer):
     promise_options = PromiseOptionSerializer(many=True, read_only=True)
     memos = MemoSerializer(many=True, read_only=True)
 
+    # 특정 username의 Mark만 반환하는 필드 추가
+    marks = serializers.SerializerMethodField()
+
     class Meta:
         model = Promise
-        fields = ['id', 'title', 'start', 'end', 'length', 'created_at', 'status', 'user', 'members', 'accept_members', 'reject_members', 'promise_options', 'memos']
+        fields = ['id', 'title', 'start', 'end', 'length', 'created_at', 'status', 'user', 'members', 'accept_members', 'reject_members', 'promise_options', 'memos', 'marks']
+
+    def get_marks(self, obj):
+        username = self.context.get('username')  # Serializer의 context에서 username 가져오기
+        marks = obj.mark.filter(user__username=username)  # 해당 username의 Mark 필터링
+        return MarkSerializer(marks, many=True).data
 
 # 가능한 약속시간 탐색 필드 검증을 위한 시리얼라이저 
 class CreatePromiseOptionsSerializer(serializers.Serializer):
