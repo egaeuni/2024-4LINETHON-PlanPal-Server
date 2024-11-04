11910d4ce5d21381f7a459f11f9ad50c54df281f
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-05 00:29:55 +0900

    Fix : 유저이름으로 약속 조회 - 중복된 객체가 조회됨 해결

--- promise/views/PromiseCRUDView.py
+++ promise/views/PromiseCRUDView.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Q
 
-from promise.models import PromiseOption, Promise
+from promise.models import Promise
 from users.models import Profile
 
 from promise.serializers import PromiseSerializer
@@ -33,6 +34,7 @@ class PromiseCRUDView(APIView):
 
         return Response({"message": "약속 정보 삭제에 성공하였습니다."}, status=status.HTTP_200_OK)
 
+# 사용자 이름으로 약속 리스트 조회
 class GETPromiseByUsername(APIView):
     # username으로 약속 정보 조회
     def get(self, request, username, format=None):
@@ -41,7 +43,7 @@ class GETPromiseByUsername(APIView):
         except Profile.DoesNotExist:
             return Response({"message": "사용자를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
 
-        promises = Promise.objects.filter(user=user)
+        promises = Promise.objects.filter(Q(user=user) | Q(accept_members=user)).distinct()
         serializer = PromiseSerializer(promises, many=True)
 
         return Response({"message": "약속 정보 조회에 성공하였습니다.", "result": serializer.data}, status=status.HTTP_200_OK)
