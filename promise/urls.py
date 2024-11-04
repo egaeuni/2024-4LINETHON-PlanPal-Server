475f7cc180d1e74d7a42c2756f9f9d95d272dc26
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-05 05:49:52 +0900

    Feat : 투표자 전체 투표했을 때 구현 완료

--- promise/urls.py
+++ promise/urls.py
@@ -3,6 +3,7 @@ from .views.CreatePromiseOptionsView import CreatePromiseOptionsView
 from .views.ConfirmImmediatelyView import ConfirmImmediatelyView
 from .views.PromiseCRUDView import PromiseCRUDView, GETPromiseByUsername
 from .views.AcceptOrRejectPromiseView import AcceptOrRejectPromiseView
+from .views.VotingView import VotingView, VotingChageStatusView
 
 urlpatterns = [
     path('promise/option/', CreatePromiseOptionsView.as_view(), name='promise-option-create'),
@@ -10,4 +11,6 @@ urlpatterns = [
     path('promise/<int:promise_id>/', PromiseCRUDView.as_view(), name='promise-crud'),
     path('promise/<str:username>/', GETPromiseByUsername.as_view(), name='promise-list-by-username'),
     path('promise/accept/<int:promise_id>/<str:username>/<str:accept_or_reject>', AcceptOrRejectPromiseView.as_view(), name='promise-accept-or-reject'),
+    path('promise/vote/<str:username>/<int:promise_id>', VotingChageStatusView.as_view(), name='promise-status-voting'),
+    path('promise/vote/<str:username>/<int:promise_id>/<int:promise_option_id>', VotingView.as_view(), name='promise-vote'),
 ]
