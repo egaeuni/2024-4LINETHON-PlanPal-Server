21ca959aa2b219784e1cb62b515148aaff056ba1
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-05 04:31:03 +0900

    Feat : 약속 내맘대로확정/수락 시 Plan 추가에 promise id 필드 추가

--- plan/serializers.py
+++ plan/serializers.py
@@ -14,16 +14,9 @@ class PlanSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     participant = ProfileSerializer(many=True, read_only=True)
     is_completed = serializers.BooleanField(required=False)
-    promise = PromiseSerializer(many=True, read_only=True)
-
+    promise_id = serializers.PrimaryKeyRelatedField(read_only=True)
+    
     class Meta:
         model = Plan
-        fields = ['id', 'title', 'category', 'start', 'end', 'participant', 'memo', 'is_completed', 'promise']
+        fields = ['id', 'title', 'category', 'start', 'end', 'participant', 'memo', 'is_completed', 'promise_id']
         read_only_fields = ['author']
-
-    # def to_representation(self, instance):
-    #     representation = super().to_representation(instance)
-    #     if instance.category:
-    #         representation['category'] = CategorySerializer(instance.category).data
-    #     representation['participant'] = [user.username for user in instance.participant.all()]
-    #     return representation
