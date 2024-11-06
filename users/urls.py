41b934b78823e3129c4ec2643d57f86ab1e45bc2
kyungminseo <110973127+mxinseo@users.noreply.github.com>
2024-11-06 21:44:07 +0900

    Revert "Revert 64 63 feat 친구 추가 무한로딩 해결"

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('friends/<str:username>/', FriendsView.as_view(), name='friends'),
-    path('profile/<str:my_username>/<str:target_username>/', ProfileView.as_view(), name='profile'),
+    path('profile/<str:username>/', ProfileView.as_view(), name='profile'),
 ]
