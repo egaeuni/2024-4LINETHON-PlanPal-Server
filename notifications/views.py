00c0cbfcb07cbef93acf010352bc39d877e2f57d
egaeuni <gaeun3619@gmail.com>
2024-11-07 18:56:34 +0900

    fix: url 패턴 수정

--- notifications/views.py
+++ notifications/views.py
@@ -15,8 +15,11 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class PlanNotificationView(APIView):
-    def get(self, request, *args, **kwargs):
+    def get(self, request, recipient_username, *args, **kwargs):
+        recipient = get_object_or_404(User, username=recipient_username)
+
         filtered_notifications = Notification.objects.filter(
+            recipient=recipient,
             notification_type__in=['plan_deadline', 'daily_achievement'])
 
         serializer = NotificationSerializer(filtered_notifications, many=True)
@@ -24,8 +27,11 @@ class PlanNotificationView(APIView):
         return Response({"message":"계획 알림을 불러왔습니다.", "result":serializer.data}, status=status.HTTP_200_OK)
 
 class PromiseNotificationView(APIView):
-    def get(self, request, *args, **kwargs):
+    def get(self, request, recipient_username, *args, **kwargs):
+        recipient = get_object_or_404(User, username=recipient_username)
+
         filtered_notifications = Notification.objects.filter(
+            recipient=recipient,
             notification_type__in=['vote', 'promise_accept'])
 
         serializer = NotificationSerializer(filtered_notifications, many=True)
@@ -33,8 +39,11 @@ class PromiseNotificationView(APIView):
         return Response({"message":"약속 알림을 불러왔습니다.", "result":serializer.data}, status=status.HTTP_200_OK)
 
 class FriendNotificationView(APIView):
-    def get(self, request, *args, **kwargs):
+    def get(self, request, recipient_username, *args, **kwargs):
+        recipient = get_object_or_404(User, username=recipient_username)
+
         filtered_notifications = Notification.objects.filter(
+            recipient=recipient,
             notification_type__in=['brag', 'cheering', 'add_friend'])
 
         serializer = NotificationSerializer(filtered_notifications, many=True)
