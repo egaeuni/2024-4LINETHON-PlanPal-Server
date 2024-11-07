64132808855173497f68ca091bb83b912e803d1a
egaeuni <gaeun3619@gmail.com>
2024-11-07 20:02:40 +0900

    fix: brag, reply url 패턴 수정

--- notifications/urls.py
+++ notifications/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('plan/<str:recipient_username>/', PlanNotificationView.as_view(), name='plan_notification'),
     path('promise/<str:recipient_username>/', PromiseNotificationView.as_view() , name='promise_notification'),
     path('friend/<str:recipient_username>/', FriendNotificationView.as_view() , name='notification_friend'),
-    path('brag/<int:plan_id>/', BragView.as_view(), name='brag'),
-    path('brags/<int:brag_id>/reply/', ReplyView.as_view(), name='reply-create'),
+    path('brag/<str:username>/<int:plan_id>/', BragView.as_view(), name='brag'),
+    path('reply/<str:username>/<int:brag_id>/', ReplyView.as_view(), name='reply-create'),
     ]
