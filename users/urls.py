1ffe52de86ca40101ed6adbc7c770a02266b3c65
egaeuni <gaeun3619@gmail.com>
2024-11-09 19:01:50 +0900

    fect: 친구 리스트

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('friends/<str:my_username>/', FriendsView.as_view(), name = 'friend-list'),
     path('friends/<str:my_username>/<str:target_username>/', FriendsView.as_view(), name='friends'),
     path('profile/<str:username>/', ProfileView.as_view(), name='profile'),
     path('username-check/', UsernameCheckView.as_view(), name='username-check'),
