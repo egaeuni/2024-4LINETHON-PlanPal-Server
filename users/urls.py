ce5057b2ad62bc05343a054c6a1b3d0b0c0ceff3
egaeuni <gaeun3619@gmail.com>
2024-11-09 16:36:51 +0900

    fect: username 중복 확인 뷰

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('friends/<str:my_username>/<str:target_username>/', FriendsView.as_view(), name='friends'),
     path('profile/<str:username>/', ProfileView.as_view(), name='profile'),
+    path('username-check/', UsernameCheckView.as_view(), name='username-check'),
 ]
