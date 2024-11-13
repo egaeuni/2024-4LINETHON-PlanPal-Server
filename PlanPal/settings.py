7d287a177100f83c85e4a6b39668a999afe4a053
egaeuni <gaeun3619@gmail.com>
2024-11-14 01:20:26 +0900

    fix: settings.py cors

--- PlanPal/settings.py
+++ PlanPal/settings.py
@@ -65,13 +65,16 @@ CORS_ORIGIN_ALLOW_ALL = True
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = "https://planpal.kro.kr"
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'PlanPal.urls'
 
