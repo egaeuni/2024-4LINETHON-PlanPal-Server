2906defea775a6d441dc9b9b345da4e84b17d15e
egaeuni <gaeun3619@gmail.com>
2024-11-15 20:20:26 +0900

    feat: friend_nickname, plan_title 불러오기

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -4,9 +4,30 @@ from users.serializers import ProfileSerializer
 from plan.serializers import PlanSerializer
 
 class NotificationSerializer(serializers.ModelSerializer):
+    plan_title = serializers.SerializerMethodField()
+    friend_nickname = serializers.SerializerMethodField()
+    
     class Meta:
         model = Notification
-        fields = ['id', 'recipient', 'message', 'notification_type']
+        fields = ['id', 'recipient', 'message', 'notification_type', 'plan_title', 'friend_nickname']
+
+    def get_plan_title(self, obj):
+        if obj.notification_type == 'cheering':
+            try:
+                reply = Reply.objects.get(id=obj.object_id)
+                return reply.brag.plan.title 
+            except Reply.DoesNotExist:
+                return None
+        return None
+
+    def get_friend_nickname(self, obj):
+        if obj.notification_type == "cheering":
+            try:
+                reply = Reply.objects.get(id=obj.object_id)
+                return reply.author.nickname
+            except Reply.DoesNotExist:
+                return None
+        return None
 
 class BragSerializer(serializers.ModelSerializer):
     author = ProfileSerializer(many=True, read_only=True)
